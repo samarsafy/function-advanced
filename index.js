391b528ed12c628d1ebb9c9cd18ce3e1169d2018
Samar <samarsafy@yahoo.de>
2018-03-27 21:46:05 +0200

    add if condition

--- index.js
+++ index.js
@@ -13,10 +13,19 @@ myFunc();
 function convertUsdToEuro() {
   "use strict";
   var amount = document.getElementById("dollar").value,
-  result = amount * 0.81,
+  result = amount * 0.81, // 100 * 0.81 = 81
   message = document.getElementById("message");
-  message.innerHTML = amount + " Dollar is worth " + result + " Euro"; 
-  if(amount === ""){
+  
+  if(amount === ""){ //amount cant be empty
     message.innerHTML = "this field cant be empty";
-  } 
+  } else if (isNaN(amount)){ // amount must be number
+    message.innerHTML = "this field accept number";
+  } else if (amount === "0"){ //amount cant be zero
+    message.innerHTML = "the value must not be zero";
+  } else if (amount < 0){ //amount cant be minus
+    message.innerHTML ="the value must not be negativ number";
+  }
+else {
+  message.innerHTML = amount + " Dollar is worth " + result + " Euro";
+}
 }
